d5a90bfd17911536e9b27d4a5b78b80b16a05684
Draed <murashkin1@yandex.ru>
2017-10-30 20:21:52 +0400

    Добавленны новые функции / Нужно исправить говнокод в switch

--- CalcLibrary/Calculator.cs
+++ CalcLibrary/Calculator.cs
@@ -4,15 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CalcLibrary
-{
-    public class Calculator
-    {
+namespace CalcLibrary {
+    public class Calculator {
 
         public string Sum(string x, string y) {
 
             double xd;
-            if(!Double.TryParse(x, out xd)) {
+            if (!Double.TryParse(x, out xd)) {
                 return "error";
             }
             double yd;
@@ -24,8 +22,79 @@ namespace CalcLibrary
             return $"{result}";
         }
 
-        string Sub(string x, string y) {
-            return "";
+        public double? Sub(string x, string y) {
+            double xd;
+            if (!Double.TryParse(x, out xd))
+                return null;
+            double yd;
+            if (!Double.TryParse(y, out yd))
+                return null;
+
+            double result = xd - yd;
+
+            return result;
+        }
+
+        public double? Div(string x, string y) {
+
+            double xd;
+            if (!Double.TryParse(x, out xd))
+                return null;
+
+            double yd;
+            if (!Double.TryParse(y, out yd) || yd == 0)
+                return null;
+
+            double result = xd / yd;
+
+            return result;
+
+        }
+
+        public double? Mul(string x, string y) {
+
+            double xd;
+            if (!Double.TryParse(x, out xd))
+                return null;
+
+            double yd;
+            if (!Double.TryParse(y, out yd))
+                return null;
+
+            double result = xd * yd;
+
+            return result;
+        }
+
+        public double? Remainder(string x, string y) {
+
+            double xd;
+            if (!Double.TryParse(x, out xd))
+                return null;
+            double yd;
+            if (!Double.TryParse(y, out yd) || yd == 0)
+                return null;
+
+            double result = xd % yd;
+
+            return result;
+        }
+
+        public double? Pow(string x, string y) {
+
+            double xd;
+            if (!Double.TryParse(x, out xd))
+                return null;
+            double yd;
+            if (!Double.TryParse(y, out yd))
+                return null;
+
+            if (xd == 0 && yd < 0)
+                return null;
+
+            double result = Math.Pow(xd, yd);
+
+            return result;
         }
 
     }
--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -9,28 +9,80 @@ namespace ConsoleApp {
 
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("HipeCalc");
+            Console.WriteLine("HipeCalc\n");
+            Console.WriteLine("Выебрите цифру\n" +
+                                "1-Сложение\n" +
+                                "2-Вычитание\n" +
+                                "3-Умножение\n" +
+                                "4-Деление\n" +
+                                "5-Остаток от деления\n" +
+                                "6-Возведение в степень\n");
 
-            var calc = new Calculator();
+            switch (Console.ReadLine()) {
 
-            if (args.Count() == 3) {
-
-                var operation = args[0];
-                var x = args[1];
-                var y = args[2];
-
-                var result = "";
-
-                if (operation == "sum") {
-
-                    result = calc.Sum(x, y);
-                }
-
-                Console.WriteLine(result);
+                case "1":
+                    Console.WriteLine("Введите 1-ое число");
+                    var operandSum1 = Console.ReadLine();
+                    Console.WriteLine("Введите 2-ое число");
+                    var opernadSum2 = Console.ReadLine();
+                    var calcSum = new Calculator();
+                    var resultSum = calcSum.Sum(operandSum1, opernadSum2);
+                    Console.WriteLine(string.Format("Результат = {0}", resultSum));
+                    break;
+                case "2":
+                    Console.WriteLine("Введите 1-ое число");
+                    var operandSub1 = Console.ReadLine();
+                    Console.WriteLine("Введите 2-ое число");
+                    var opernadSub2 = Console.ReadLine();
+                    var calcSub = new Calculator();
+                    var resultSub = calcSub.Sub(operandSub1, opernadSub2);
+                    Console.WriteLine(string.Format("Результат = {0}", resultSub));
+                    break;
+                case "3":
+                    Console.WriteLine("Введите 1-ое число");
+                    var operandMul1 = Console.ReadLine();
+                    Console.WriteLine("Введите 2-ое число");
+                    var opernadMul2 = Console.ReadLine();
+                    var calcMul = new Calculator();
+                    var resultMul = calcMul.Mul(operandMul1, opernadMul2);
+                    Console.WriteLine(string.Format("Результат = {0}", resultMul));
+                    break;
+                case "4":
+                    Console.WriteLine("Введите 1-ое число");
+                    var operandDiv1 = Console.ReadLine();
+                    Console.WriteLine("Введите 2-ое число");
+                    var opernadDiv2 = Console.ReadLine();
+                    var calcDiv = new Calculator();
+                    var resultDiv = calcDiv.Div(operandDiv1, opernadDiv2);
+                    Console.WriteLine(string.Format("Результат = {0}", resultDiv));
+                    break;
+                case "5":
+                    Console.WriteLine("Введите 1-ое число");
+                    var operandRemainder1 = Console.ReadLine();
+                    Console.WriteLine("Введите 2-ое число");
+                    var opernadRemainder2 = Console.ReadLine();
+                    var calcRemainder = new Calculator();
+                    var resultRemainder = calcRemainder.Remainder(operandRemainder1, opernadRemainder2);
+                    Console.WriteLine(string.Format("Результат = {0}", resultRemainder));
+                    break;
+                case "6":
+                    Console.WriteLine("Введите 1-ое число");
+                    var operandPow1 = Console.ReadLine();
+                    Console.WriteLine("Введите 2-ое число");
+                    var opernadPow2 = Console.ReadLine();
+                    var calcPow = new Calculator();
+                    var resultPow = calcPow.Pow(operandPow1, opernadPow2);
+                    Console.WriteLine(string.Format("Результат = {0}", resultPow));
+                    break;
+                default:
+                    Console.WriteLine("Такой цифры нет");
+                    break;
             }
+
 
             Console.ReadLine();
         }
+
     }
 
 }
