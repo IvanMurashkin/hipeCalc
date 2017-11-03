4a616a3a2a268d928c149df2e3219aa466fae557
Draed <murashkin1@yandex.ru>
2017-11-03 10:48:25 +0400

    Допилить вытаскивание из бд тяжелых вычислений

---
+++ DBModel/Model/OperationHistory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBModel.Model {
+    public class OperationHistory {
+
+        public OperationHistory() {}
+
+        public OperationHistory(string name, 
+                                string args, 
+                                double? result, 
+                                long excecTime) {
+            Name = name;
+            Args = args;
+            Result = result;
+            ExcecTime = excecTime;
+                
+        }
+
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Args { get; set; }
+        public double? Result { get; set; }
+        public long ExcecTime { get; set; }
+
+    }
+}
